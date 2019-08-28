019f3f68604069aceb04ffc0a6f5cd6b9b87efac
svegal <52968540+svegal@users.noreply.github.com>
2019-08-28 14:25:42 -0500

    Add files via upload

---
+++ talleres/taller07/Nodo.java
@@ -0,0 +1,12 @@
+
+/**
+ * 
+ * @author (Isabella Quintero, Sofia Vega) 
+ * @version (27/08/19)
+ */
+public class Nodo {
+    public Nodo(int e){ dato = e;}
+    public int dato;
+    public Nodo elSiguienteDespuesDeMi;
+}
+
