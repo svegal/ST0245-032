e12028483326d584d99062056ab8c54106b4a542
iquinterov <52968547+iquinterov@users.noreply.github.com>
2019-09-17 11:11:18 -0500

    Add files via upload

---
+++ talleres/taller09/UnaTablaDeHash.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Taller__9;
+
+/**
+* La clase UnaTablaDeHash, es un plantilla para crear nuestra propia estructura hash
+* Dentro de sus características esta que tiene un string como llave (key) y un entero
+* como valor (value)
+* @author Mauricio Toro, Andres Paez
+* @version 1
+*/
+public class UnaTablaDeHash
+{
+   /*
+   * Utilizamos array por simplicidad sin embargo el uso de este no permite manejar coliciones
+   * de manera eficiente, por lo cual esta primera versión de hash no implementa colisiones, esta
+   * característica se implementará posteriormente.
+   */
+   private int[] tabla;
+
+
+   /**
+   * Este método define el array utilizado con 10 posiciones de manera que tenemos un límite de 10 elementos
+   * 
+   */
+   public UnaTablaDeHash(){
+      tabla = new int[10];
+   }
+
+   /**
+   * @param k es la llave del elemento que servirá como referencia al buscar
+   * Este método se utiliza para darle una posicion dentro de la tabla, donde el %10 asegura que solo
+   * se utilicen digitos de 0 a 10.
+   */
+   private int funcionHash(String k){
+       return ((int) k.charAt(0)) % 10;
+   }
+
+   /**
+   * @param k es la llave del elemento que servirá como referencia al buscar
+   * Este método se utiliza para obtener el elmento con la clave k, se enlaza con funcionHash
+   */
+   public int get(String k){
+       int posicion = funcionHash(k);
+       int numeroTel = tabla[posicion];
+       return numeroTel;
+   }
+
+   /**
+   * @param k es la llave del elemento que servirá como referencia al buscar posteriormente
+   * @param v es el valor asociado a la llave k
+   * Este método se utiliza para agregar un nuevo elemento
+   */
+   public void put(String k, int v){
+       int posicion = funcionHash(k);
+       tabla[posicion] = v;
+   }
+}
