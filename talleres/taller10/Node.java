7078c2041bc6fe5fc8cda0d710b3c517999f493e
iquinterov <52968547+iquinterov@users.noreply.github.com>
2019-09-24 11:59:29 -0500

    Add files via upload

---
+++ talleres/taller10/Node.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller_10;
+
+/**
+ *
+ * @author cl18417
+ */
+public class Node {
+ public Node left;
+ public Node right;
+ public int data;
+ public Node(int d){
+	 data = d;
+ }
+}
